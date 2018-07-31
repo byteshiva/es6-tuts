c65063bdbc06d8ed1f6575ad99fb28ee49ed1f67
Siva <byteshiva@users.noreply.github.com>
2018-08-01 02:59:20 +0530

    Update index.mjs

--- media-stream/websrtc/index.mjs
+++ media-stream/websrtc/index.mjs
@@ -1,10 +1,28 @@
-const puppeteer = import('puppeteer');
+import puppeteer from 'puppeteer';
 import * as data from './config/config.json';
 const conf = data.default;
-const {phoneToDial,oneSec,threeSec,twoMin,oneMin,siteForDial,countryName,phoneDialerSelector,dialpadAreaSelector,phoneElemSelector,areaCodeSelector,phoneNumSelector,btnCallSelector,beforeDialCallScrnShotPath,afterDialCallScrnShotPath} = conf;
+const {
+    phoneToDial,
+    oneSec,
+    threeSec,
+    twoMin,
+    oneMin,
+    siteForDial,
+    countryName,
+    phoneDialerSelector,
+    dialpadAreaSelector,
+    phoneElemSelector,
+    areaCodeSelector,
+    phoneNumSelector,
+    btnCallSelector,
+    beforeDialCallScrnShotPath,
+    afterDialCallScrnShotPath
+} = conf;
 
 
 (async () => {
+
+    console.log(puppeteer);
     // Enable media stream
     // https://peter.sh/experiments/chromium-command-line-switches/
     const browser = await puppeteer.launch({
@@ -23,14 +41,17 @@ const {phoneToDial,oneSec,threeSec,twoMin,oneMin,siteForDial,countryName,phoneDi
     await (() => console.log(`after ${threeSec} secs`));
 
     // Enter the number to be dial
-    await page.$(dialpadArea).then(() => console.log("page loaded"));
+    await page.$(dialpadAreaSelector).then(() => console.log("page loaded"));
     const phoneElement = await page.$(phoneElemSelector);
     await phoneElement.click();
 
     // Example -- select a value from dropdown list if there exist one to get ISD Area code
-    const selectElem = await page.$(areaCodeSelector);
-    selectElem.type(countryName);
-
+    try {
+        const selectElem = await page.$(areaCodeSelector);
+        selectElem.type(countryName);
+    } catch (e) {
+        console.log('[ selectElem ]', e)
+    }
     // Wait until the dom gets loaded.
     await page.waitFor(threeSec);
     // Enter the phone to dial 
