9f490074fe333c0603031429d770ccec3a33f9c6
Siva <byteshiva@users.noreply.github.com>
2018-08-02 20:13:31 +0530

    Create stack.js

---
+++ datastructures/stack.js
@@ -0,0 +1,33 @@
+var Stack = (function(){
+
+    var stack = [],
+        allowed = [ "push", "pop", "length" ];
+    
+    var target = {};
+    var handler = {
+           get (receiver, name){;
+            if (allowed.indexOf(name) > -1){
+                if(typeof stack[name] == "function"){
+                    return stack[name].bind(stack);
+                } else {
+                    return stack[name];
+                }
+            } else {
+                return undefined;
+            }
+        }
+    };
+
+  return new Proxy(target, handler);
+
+});
+
+var mystack = new Stack();
+
+mystack.push("Test");
+mystack.push("Data 2");
+
+console.log(mystack.length);    //1
+
+console.log(mystack[0]);        //undefined
+console.log(mystack.pop()); //"Data 2"
