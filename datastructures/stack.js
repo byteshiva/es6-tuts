72f221aacf761483b701e9cb158a94bd5b1928f3
Siva <byteshiva@users.noreply.github.com>
2018-08-03 01:03:19 +0530

    Update stack.js

--- datastructures/stack.js
+++ datastructures/stack.js
@@ -1,28 +1,58 @@
-var Stack = (function(){
-
-    var stack = [],
-        allowed = [ "push", "pop", "length" ];
+// Third version to develop Stack 
+// using arrow functions with Custom Stack Implemetation using Array
+var Stack = ((data = [], t = 0) => {
+    let dataStore = data,
+        top = t,
+        stackProto = {
+            push (element) {
+                dataStore[top++] = element
+            },
+            pop: () => dataStore.splice(--top, 1),
+            peek: () => dataStore[top - 1],
+            length: () => top,
+            isEmpty: () => top === 0,
+            getStack: () => dataStore,
+            getStackElems: () => console.log(...Array.from(dataStore))
+        },
+        target = Object.create(stackProto),
+        allowed = [ "push", "pop", "peek", "length", "isEmpty", "getStack" ];
     
-    var target = {};
-    var handler = {
-           get (receiver, name){
-            if (allowed.indexOf(name) > -1){
-                if(typeof stack[name] == "function"){
-                    return stack[name].bind(stack);
-                } else {
-                    return stack[name];
-                }
-            } else {
-                return undefined;
+    const invariant = (key, action) => {
+        const errMsg = `Invalid attempt to ${action} private "${key}" property`;
+        if(action == "get") {
+            if (key[0] === '_') {
+                throw new Error(errMsg);
             }
+        }
+
+        if (action == "delete") {
+            if (key[0] === '_' || (allowed.indexOf(key) > -1)) {
+               throw new Error(errMsg);
+            }   
         }
     };
 
-  return new Proxy(target, handler);
+    const handler = {
+        get: (receiver, key) => {
+            invariant(key, 'get');
+            return (allowed.indexOf(key) > -1) ? receiver[key] : undefined;
+         },
+         set: (receiver, key, value) => {
+             console.log('[key] - [value] ', key, value);
+             return true;
+         },
+         deleteProperty: (target, key) => {
+             invariant(key, 'delete')
+             return true
+         }
+    }
 
+  return new Proxy(target, handler);
 });
 
-var mystack = new Stack();
+
+
+var mystack = Stack();
 
 mystack.push("Test");
 mystack.push("Data 2");
@@ -32,37 +62,9 @@ console.log(mystack.length);    //1
 console.log(mystack[0]);        //undefined
 console.log(mystack.pop()); //"Data 2"
 
-// Second version Stack Implementation using Proxy
-// Use Arrow Functions
-// Second version to develop Stack 
-// using arrow functions
-var Stack = (() => {
-    let stack = [],
-        target = {},
-        allowed = [ "push", "pop", "length" ];
-    
-    const invariant = (key, action) => {
-        if (key[0] === '_') {
-            throw new Error(`Invalid attempt to ${action} private "${key}" property`);
-        }
-    };
+delete mystack.pop
+// Error: Invalid attempt to delete private "pop" property
 
-    // This check is introduced to return length 
-    const checkTypeFnNum = (key) => {
-        return (typeof stack[key] === "function") ? stack[key].bind(stack) : stack[key];
-    };
-
-    const handler = {
-           get: (receiver, key) => {
-               invariant(key, 'get');
-               return (allowed.indexOf(key) > -1) ? checkTypeFnNum(key) : undefined;
-            },
-            set: (receiver, key, value) => {
-                console.log('[key] - [value] ', key, value);
-                return true;
-            }
-        }
-
-  return new Proxy(target, handler);
-});
+mystack.__proto__
+// Error: Invalid attempt to get private "__proto__" property
 
