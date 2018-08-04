b057bbcfd93b7d7cf0e41fb42ba973931e711e0d
Siva <byteshiva@users.noreply.github.com>
2018-08-04 22:43:02 +0530

    Update index.js

--- problems-solving/year/2018/month/aug/day04/index.js
+++ problems-solving/year/2018/month/aug/day04/index.js
@@ -11,5 +11,26 @@ uniqueCharacters = (str) => {
     return true;
 }
  
+// Find unique string without use additional data structure.
+// usage of charAtr
+uniqCharStr = (str) => {
+   let count = 0;
+   for (i = 1; i < str.length; i++)  {
+     count++;
+     for (j = 0; j < i; j++) {
+         count++;
+         if (str.charAt(i) == str.charAt(j)) {
+             console.log("[ count - false ]", count);
+             return false;
+         }
+     }
+   }
+    console.log("[ count - true ]", count);
+    return true;   
+}
+ 
+console.log(uniqCharStr("abcdef"));
+console.log(uniqCharStr("abcdefaa"));
+
 console.log(uniqueCharacters("abcdef"));
 console.log(uniqueCharacters("aabcdef"));
