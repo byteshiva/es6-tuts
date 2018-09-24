6a2df0277231776f2a4726c7bbb33a3bcb657946
Siva <byteshiva@users.noreply.github.com>
2018-09-24 09:52:11 +0530

    Add files via upload

---
+++ functionalprogramming/day6/funcday6/index.js
@@ -0,0 +1,6 @@
+const  R = require('ramda');
+
+var mapIndexed = R.addIndex(R.map);
+const = mapInd = mapIndexed((val, idx) => idx + '-' + val, ['f', 'o', 'o', 'b', 'a', 'r']);
+
+console.log(mapInd);
