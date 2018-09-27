be07266252808ac3f58aff88cbc80755db5c6223
Siva <byteshiva@users.noreply.github.com>
2018-09-27 10:01:27 +0530

    Add files via upload

---
+++ functionalprogramming/day9/funcday8/index.js
@@ -0,0 +1,10 @@
+const R = require('ramda');
+
+var isQueen = R.propEq('rank', 'Q');
+var isSpade = R.propEq('suit', '♠︎');
+var isQueenOfSpades = R.allPass([isQueen, isSpade]);
+
+const predicateCase1 = isQueenOfSpades({rank: 'Q', suit: '♣︎'}); //=> false
+const predicateCase2 = sQueenOfSpades({rank: 'Q', suit: '♠︎'}); //=> true
+
+console.log(predicateCase1, predicateCase2);
