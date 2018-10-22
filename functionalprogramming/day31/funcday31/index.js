579fe93a54ee95c4b1b0cd66ba434a250dc62efe
Siva <byteshiva@users.noreply.github.com>
2018-10-22 11:21:00 +0530

    Add files via upload

---
+++ functionalprogramming/day31/funcday31/index.js
@@ -0,0 +1,10 @@
+const R = require('ramda');
+
+var isNotNil = R.complement(R.isNil);
+const sNNl = isNil(null); //=> true
+const sNotNl = isNotNil(null); //=> false
+const sNNl7 = isNil(7); //=> false
+const sNNull7 = isNotNil(7); //=> true
+
+console.log(sNNl, sNotNl, sNNl7, sNNull7);
+
