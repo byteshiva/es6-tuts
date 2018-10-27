f589f1066a26e3045b2438b4e0055f1079a630b2
Siva <byteshiva@users.noreply.github.com>
2018-10-27 08:13:28 +0530

    Add files via upload

--- functionalprogramming/day36/funcday36/index.js
+++ functionalprogramming/day36/funcday36/index.js
@@ -5,8 +5,8 @@ var fn = R.cond([
   [R.equals(100), R.always('water boils at 100°C')],
   [R.T,           temp => 'nothing special happens at ' + temp + '°C']
 ]);
-console.log("water freezes at ", fn(0)); //=> 'water freezes at 0°C'
-console.log("nothing special happens at ", fn(50)); //=> 'nothing special happens at 50°C'
-console.log("water boils at ", fn(100)); //=> 'water boils at 100°C'
+console.log(fn(0)); //=> 'water freezes at 0°C'
+console.log(fn(50)); //=> 'nothing special happens at 50°C'
+console.log(fn(100)); //=> 'water boils at 100°C'
 
 
