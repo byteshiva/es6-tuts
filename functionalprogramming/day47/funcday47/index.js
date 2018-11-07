54b7e42106e538888447e5fad7324409d10f8833
Siva <byteshiva@users.noreply.github.com>
2018-11-07 11:44:25 +0530

    Add files via upload

--- functionalprogramming/day47/funcday47/index.js
+++ functionalprogramming/day47/funcday47/index.js
@@ -4,4 +4,4 @@ console.log(R.difference([1,2,3,4], [7,6,5,4,3]));
 
 console.log(R.difference([7,6,5,4,3], [1,2,3,4]));
 
-console.log(R.difference([{a:1}, {b:2}], [{a: 1}, {c: 3}]);
+console.log(R.difference([{a:1}, {b:2}], [{a: 1}, {c: 3}]));
