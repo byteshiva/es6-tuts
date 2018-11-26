b8934cc03301e1a07709db3559f8f955e590c25e
Siva <byteshiva@users.noreply.github.com>
2018-11-26 11:41:33 +0530

    Add files via upload

--- functionalprogramming/day66/funcday66/index.js
+++ functionalprogramming/day66/funcday66/index.js
@@ -2,6 +2,6 @@ const R = require('ramda');
 
 const xs = [{a: 1}, {a: 2}, {a: 3}];
 const rfndindx1 = R.findIndex(R.propEq('a', 2))(xs); //=> 1
-const rfindindx2 = R.findIndex(R.propEq('a', 4))(xs); //=> -1
+const rfndindx2 = R.findIndex(R.propEq('a', 4))(xs); //=> -1
 
-console.log(rfindindx1, rfindindx2);
+console.log(rfndindx1, rfndindx2);
