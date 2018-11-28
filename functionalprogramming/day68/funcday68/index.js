e213bbc851cb6439ba71f48a85e6b8302ccca45b
Siva <byteshiva@users.noreply.github.com>
2018-11-28 08:12:31 +0530

    Add files via upload

--- functionalprogramming/day68/funcday68/index.js
+++ functionalprogramming/day68/funcday68/index.js
@@ -4,4 +4,4 @@ const xs = [{a: 1, b: 0}, {a:1, b: 1}];
 const rfindlast1 = R.findLast(R.propEq('a', 1))(xs); //=> {a: 1, b: 1}
 const rfindlast2 = R.findLast(R.propEq('a', 4))(xs); //=> undefined
 
-console.log(rfindlast1, rfindLast2);
+console.log(rfindlast1, rfindlast2);
