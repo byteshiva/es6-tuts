88aa0ee8af0fee212517c2eeb4a4381bcdc917de
Siva <byteshiva@users.noreply.github.com>
2018-12-01 02:32:15 +0530

    Add files via upload

---
+++ functionalprogramming/day71/funcday71/index.js
@@ -0,0 +1,10 @@
+const R = require('ramda');
+
+const mergeThree = (a, b, c) => [].concat(a, b, c);
+
+const merge3 = mergeThree(1, 2, 3); //=> [1, 2, 3]
+
+const flipmerge3 = R.flip(mergeThree)(1, 2, 3); //=> [2, 1, 3]
+
+console.log(mergeThree, merge3);
+
