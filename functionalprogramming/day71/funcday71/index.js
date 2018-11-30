9c1efa752a43803dfb6a4ea6dbbc9d35ce848519
Siva <byteshiva@users.noreply.github.com>
2018-12-01 02:33:20 +0530

    Add files via upload

--- functionalprogramming/day71/funcday71/index.js
+++ functionalprogramming/day71/funcday71/index.js
@@ -6,5 +6,5 @@ const merge3 = mergeThree(1, 2, 3); //=> [1, 2, 3]
 
 const flipmerge3 = R.flip(mergeThree)(1, 2, 3); //=> [2, 1, 3]
 
-console.log(mergeThree, merge3);
+console.log(flipmerge3, merge3);
 
