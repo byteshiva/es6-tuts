a2917030d3da87353505a39475cf02a761531f7b
Siva <byteshiva@users.noreply.github.com>
2018-12-05 08:20:47 +0530

    Add files via upload

---
+++ functionalprogramming/day75/funcday75/index.js
@@ -0,0 +1,16 @@
+const R = require('ramda');
+
+
+const byGrade = R.groupBy(function(student) {
+  const score = student.score;
+  return score < 65 ? 'F' :
+         score < 70 ? 'D' :
+         score < 80 ? 'C' :
+         score < 90 ? 'B' : 'A';
+});
+const students = [{name: 'Abby', score: 84},
+                {name: 'Eddy', score: 58},
+                {name: 'Jack', score: 69}];
+const bgrade = byGrade(students);
+
+console.log(bgrade);
