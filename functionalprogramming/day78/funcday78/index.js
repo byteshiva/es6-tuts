445d4954645a5f006f3942bc9d9550473a8ac89b
Siva <byteshiva@users.noreply.github.com>
2018-12-08 07:23:28 +0530

    Add files via upload

---
+++ functionalprogramming/day78/funcday78/index.js
@@ -0,0 +1,9 @@
+const R = require('ramda');
+
+const rgte1 = R.gte(2, 1); //=> true
+const rgte2 = R.gte(2, 2); //=> true
+const rgte3 = R.gte(2, 3); //=> false
+const rgte4 = R.gte('a', 'z'); //=> false
+const rgte5 = R.gte('z', 'a'); //=> true
+
+console.log(rgte1, rget2, rgte3, rgte4, rgte5);
