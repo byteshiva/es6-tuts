6ed411f3f19bee3d75cb4e66b8b971aa28e704bb
Siva <byteshiva@users.noreply.github.com>
2018-12-19 09:50:46 +0530

    Add files via upload

---
+++ functionalprogramming/day89/index.js
@@ -0,0 +1,7 @@
+const R = require('ramda');
+
+const list = [{id: 'xyz', title: 'A'}, {id: 'abc', title: 'B'}];
+const rindexby1 = R.indexBy(R.prop('id'), list);
+
+console.log(rindexby1);
+//=> {abc: {id: 'abc', title: 'B'}, xyz: {id: 'xyz', title: 'A'}}
