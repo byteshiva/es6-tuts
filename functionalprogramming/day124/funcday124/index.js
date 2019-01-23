733049115188c0df8fa59d1fc3d8c8450e9de81d
Siva <byteshiva@users.noreply.github.com>
2019-01-23 09:43:44 +0530

    Add files via upload

--- functionalprogramming/day124/funcday124/index.js
+++ functionalprogramming/day124/funcday124/index.js
@@ -2,7 +2,6 @@ const R = require('ramda');
 
 const rmatch1 = R.match(/([a-z]a)/g, 'bananas'); //=> ['ba', 'na', 'na']
 const rmatch2 = R.match(/a/, 'b'); //=> []
-const rmatch3 = R.match(/a/, null); //=> TypeError: null does not have a method named "match"
+// const rmatch3 = R.match(/a/, null); //=> TypeError: null does not have a method named "match"
 
-console.log(rmatch1, rmatch2, rmatch3);
-
+console.log(rmatch1, rmatch2); // , rmatch3);
