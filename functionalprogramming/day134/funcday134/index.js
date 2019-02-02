c5fa5f75f37ac69a117a98eac6edd2f30ae7bd6e
Siva <byteshiva@users.noreply.github.com>
2019-02-02 18:05:56 +0530

    Add files via upload

---
+++ functionalprogramming/day134/funcday134/index.js
@@ -0,0 +1,7 @@
+const R = require('ramda');
+
+const rmergedeepright1 = R.mergeDeepRight({ name: 'fred', age: 10, contact: { email: 'moo@example.com' }},
+                 { age: 40, contact: { email: 'baa@example.com' }});
+//=> { name: 'fred', age: 40, contact: { email: 'baa@example.com' }}
+//
+console.log(rmergedeepright1);
