105b73b2bb17d5ae04b9183b6ccd71ad5797f8dc
Siva <byteshiva@users.noreply.github.com>
2019-02-12 11:28:01 +0530

    Add files via upload

---
+++ functionalprogramming/day144/funcday144/index.js
@@ -0,0 +1,6 @@
+const R = require('ramda');
+
+const rmove1 = R.move(0, 2, ['a', 'b', 'c', 'd', 'e', 'f']); //=> ['b', 'c', 'a', 'd', 'e', 'f']
+const rmove2 = R.move(-1, 0, ['a', 'b', 'c', 'd', 'e', 'f']); //=> ['f', 'a', 'b', 'c', 'd', 'e'] list rotation
+
+console.log(rmove1, rmove2);
