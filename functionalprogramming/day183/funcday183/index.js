866b048aa2b556f48cebb31b1ec00d69405046df
Siva <byteshiva@users.noreply.github.com>
2019-03-23 10:45:06 +0530

    Add files via upload

---
+++ functionalprogramming/day183/funcday183/index.js
@@ -0,0 +1,14 @@
+const R = require('ramda');
+
+cosnt rreduce1 = R.reduce(R.subtract, 0, [1, 2, 3, 4]) // => ((((0 - 1) - 2) - 3) - 4) = -10
+
+console.log(rreduce1);
+//          -               -10
+//         / \              / \
+//        -   4           -6   4
+//       / \              / \
+//      -   3   ==>     -3   3
+//     / \              / \
+//    -   2           -1   2
+//   / \              / \
+//  0   1            0   1
