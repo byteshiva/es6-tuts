6f3115b942f92823cea61b64806967b95b691060
Siva <byteshiva@users.noreply.github.com>
2019-05-06 04:42:56 +0530

    Add files via upload

--- functionalprogramming/day225/funcday225/index.js
+++ functionalprogramming/day225/funcday225/index.js
@@ -1,10 +1,10 @@
 const R = require('ramda');
-const M       = require('ramda-fantasy').Maybe;
-const Just    = M.Just;
-const Nothing = M.Nothing;
+const Maybe       = require('ramda-fantasy').Maybe;
+const Just    = Maybe.Just;
+const Nothing = Maybe.Nothing;
 
 // Returns `Maybe.Nothing` if the given divisor is `0`
-const safeDiv = n => d => d === 0 ? Maybe.Nothing() : Maybe.Just(n / d)
+const safeDiv = n => d => d === 0 ? Nothing() : Just(n / d)
 
 const rtraverse1 = R.traverse(Maybe.of, safeDiv(10), [2, 4, 5]); //=> Maybe.Just([5, 2.5, 2])
 const rtraverse2 = R.traverse(Maybe.of, safeDiv(10), [2, 0, 5]); //=> Maybe.Nothing
