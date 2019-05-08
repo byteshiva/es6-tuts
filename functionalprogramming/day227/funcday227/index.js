6c64e6432eb4579b4e7cc007813f06238a5c890e
Siva <byteshiva@users.noreply.github.com>
2019-05-08 15:41:36 +0530

    Add files via upload

---
+++ functionalprogramming/day227/funcday227/index.js
@@ -0,0 +1,8 @@
+const R = requrie('ramda');
+
+const rtrycatch1 = R.tryCatch(R.prop('x'), R.F)({x: true}); //=> true
+const rtrycatch2 = R.tryCatch(() => { throw 'foo'}, R.always('catched'))('bar') // => 'catched'
+const rtrycatch3 = R.tryCatch(R.times(R.identity), R.always([]))('s') // => []
+
+
+console.log(rtrycatch1, rtrycatch2, rtrycatch3);
