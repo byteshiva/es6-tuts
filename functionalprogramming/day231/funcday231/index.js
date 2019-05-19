610ac26804422e86ee21b83e8cda4356fe566dc6
Siva <byteshiva@users.noreply.github.com>
2019-05-19 14:14:04 +0530

    Add files via upload

--- functionalprogramming/day231/funcday231/index.js
+++ functionalprogramming/day231/funcday231/index.js
@@ -3,6 +3,6 @@ const R = require('ramda');
 const addFour = a => b => c => d => a + b + c + d;
 
 const uncurriedAddFour = R.uncurryN(4, addFour);
-const runcurry1 = uncurriedAddFour(1, 2, 3, 4); //=> 10
+const runcurried1 = uncurriedAddFour(1, 2, 3, 4); //=> 10
 
-console.log(runcurry1);
+console.log(runcurried1);
